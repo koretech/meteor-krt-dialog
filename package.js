27e62e569368a55cddafbba516e088583737f8ba
Mike Kornelson <mike@koretech.ca>
2015-02-27 16:10:48 -0600

    bump

--- package.js
+++ package.js
@@ -3,8 +3,9 @@ var client = 'client', server = 'server', both = ['client', 'server'];
 Package.describe({
 	name: 'krt:dialog',
 	summary: 'Koretech Dialog Package',
-	version: '0.1.1',
-	git: 'https://github.com/koretech/meteor-krt-dialog.git'
+	version: '0.1.2',
+	git: 'https://github.com/koretech/meteor-krt-dialog.git',
+	documentation: null
 });
 
 Package.onUse(function(api){
@@ -12,15 +13,15 @@ Package.onUse(function(api){
 	api.versionsFrom('METEOR@1.0');
 
 	api.use([
-		'krt:core@0.1.0',
+		'krt:core@0.1.2',
 		'templating',
 		'underscore',
-		'nooitaf:semantic-ui@1.0.1'
+		'nooitaf:semantic-ui@1.9.1'
 	], both);
 
 	api.imply([
 		'krt:core',
-		'nooitaf:semantic-ui'
+		'nooitaf:semantic-ui@1.9.1'
 	]);
 
 	api.addFiles([
