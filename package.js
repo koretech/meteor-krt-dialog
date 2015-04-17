0ebf10eba04fe39809c4281c415243dd25fc4db3
Mike Kornelson <mike@koretech.ca>
2015-04-17 09:11:25 -0500

    Dialog Class ============ Added the events param Reworked the onVisible function
    AreYouSure
    ==========
    Added the krtDialogAreYouSure function

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ var client = 'client', server = 'server', both = ['client', 'server'];
 Package.describe({
 	name: 'krt:dialog',
 	summary: 'Koretech Dialog Package',
-	version: '0.1.2',
+	version: '0.1.4',
 	git: 'https://github.com/koretech/meteor-krt-dialog.git',
 	documentation: null
 });
@@ -13,15 +13,15 @@ Package.onUse(function(api){
 	api.versionsFrom('METEOR@1.0');
 
 	api.use([
-		'krt:core@0.1.2',
+		'krt:core@0.1.3',
 		'templating',
 		'underscore',
-		'nooitaf:semantic-ui@1.9.1'
+		'semantic:ui-css@1.12.0'
 	], both);
 
 	api.imply([
 		'krt:core',
-		'nooitaf:semantic-ui@1.9.1'
+		'semantic:ui-css'
 	]);
 
 	api.addFiles([
@@ -29,7 +29,9 @@ Package.onUse(function(api){
 	], both);
 
 	api.addFiles([
-		'lib/dialog.js'
+		'lib/dialog.js',
+		'lib/defaults.html',
+		'lib/defaults.js'
 	], client);
 
 });
