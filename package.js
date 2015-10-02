df0da83b924d293ed2e8b48dd15c8acde1eca05d
Mike Kornelson <mike@koretech.ca>
2015-10-02 11:22:32 -0500

    Updated to Meteor 1.2

--- package.js
+++ package.js
@@ -3,17 +3,17 @@ var client = 'client', server = 'server', both = ['client', 'server'];
 Package.describe({
 	name: 'krt:dialog',
 	summary: 'Koretech Dialog Package',
-	version: '0.2.0',
+	version: '0.2.1',
 	git: 'https://github.com/koretech/meteor-krt-dialog.git',
 	documentation: null
 });
 
 Package.onUse(function(api){
 
-	api.versionsFrom('METEOR@1.0');
+	api.versionsFrom('METEOR@1.2');
 
 	api.use([
-		'krt:core@0.1.3',
+		'krt:core@0.1.4',
 		'templating',
 		'underscore'
 	], both);
